d0c6845d9762c797a3034d5fb47055561fd16811
Ian k Armstrong <ian.k.armstrong@gmail.com>
2020-11-17 08:18:26 +0200

    17.11.2020

--- lib/detailPage.dart
+++ lib/detailPage.dart
@@ -7,44 +7,44 @@ import 'package:provider/provider.dart';
 
 // https://www.linkedin.com/pulse/to-do-list-flutter-sqlite-local-database-udara-abeythilake
 
-class Index with ChangeNotifier {
-  int indx;
+// class Index with ChangeNotifier {
+//   int indx;
+//
+//   Index({this.indx});
+//
+//   void Change(int i) {
+//     indx = i;
+//     notifyListeners();
+//   }
+// }
 
-  Index({this.indx});
-
-  void Change(int i) {
-    indx = i;
-    notifyListeners();
-  }
-}
+// class DetailPage extends StatelessWidget {
+//   List<Chapter> chapters;
+//   int index;
+//
+//   DetailPage(chapters, int index) {
+//     this.chapters = chapters;
+//     this.index = index;
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(
+//           create: (context) => Index(indx: this.index),
+//         ),
+//       ],
+//       child: Page(this.chapters, index),
+//     );
+//   }
+// }
 
 class DetailPage extends StatelessWidget {
   List<Chapter> chapters;
   int index;
 
   DetailPage(chapters, int index) {
-    this.chapters = chapters;
-    this.index = index;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => Index(indx: this.index),
-        ),
-      ],
-      child: Page(this.chapters, index),
-    );
-  }
-}
-
-class Page extends StatelessWidget {
-  List<Chapter> chapters;
-  int index;
-
-  Page(chapters, int index) {
     this.chapters = chapters;
     this.index = index;
   }
@@ -61,177 +61,179 @@ class Page extends StatelessWidget {
         fontSize: FontSize(16.0));
 
     final h4 = Style(fontSize: FontSize(18.0));
+    final h3 = Style(fontSize: FontSize(16.0));
 
     final page0 = Html(
       data: chapters[0].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page1 = Html(
       data: chapters[1].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page2 = Html(
       data: chapters[2].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page3 = Html(
       data: chapters[3].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page4 = Html(
       data: chapters[4].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page5 = Html(
       data: chapters[5].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page6 = Html(
       data: chapters[6].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page7 = Html(
       data: chapters[7].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page8 = Html(
       data: chapters[8].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page9 = Html(
       data: chapters[9].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page10 = Html(
       data: chapters[10].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page11 = Html(
       data: chapters[11].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page12 = Html(
       data: chapters[12].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page13 = Html(
       data: chapters[13].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page14 = Html(
       data: chapters[14].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page15 = Html(
       data: chapters[15].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page16 = Html(
       data: chapters[16].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page17 = Html(
       data: chapters[17].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page18 = Html(
       data: chapters[18].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page19 = Html(
       data: chapters[19].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page20 = Html(
       data: chapters[20].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page21 = Html(
       data: chapters[21].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page22 = Html(
       data: chapters[22].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page23 = Html(
       data: chapters[23].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page24 = Html(
       data: chapters[24].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page25 = Html(
       data: chapters[25].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page26 = Html(
       data: chapters[26].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page27 = Html(
       data: chapters[27].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page28 = Html(
       data: chapters[28].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page29 = Html(
       data: chapters[29].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page30 = Html(
       data: chapters[30].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page31 = Html(
       data: chapters[31].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final page32 = Html(
       data: chapters[32].text,
-      style: {"html": html, "h4": h4},
+      style: {"html": html, "h4": h4, "h3": h3},
     );
 
     final topAppBar = AppBar(
           elevation: 0.1,
           backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
-          title: Text(
-              "Capítulo ${Provider.of<Index>(context, listen: true).indx + 1}"),
+          title: Text('la Confesión de Fe de Westminster'),
+          // title: Text(
+          //     "Capítulo ${Provider.of<Index>(context, listen: true).indx + 1}"),
           centerTitle: true,
           // actions: [
           //   IconButton(
@@ -257,9 +259,9 @@ class Page extends StatelessWidget {
           controller: pageController,
           scrollDirection: Axis.horizontal,
           pageSnapping: true,
-          onPageChanged: (num) {
-            Provider.of<Index>(context, listen: false).Change(num);
-          },
+          // onPageChanged: (num) {
+          //   Provider.of<Index>(context, listen: false).Change(num);
+          // },
           children: [
             Container(
                 child: SingleChildScrollView(
