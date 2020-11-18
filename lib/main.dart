72ad15b7a5c45a9fc0a3e6d6c943be75c4525ab7
Ian k Armstrong <ian.k.armstrong@gmail.com>
2020-11-18 08:40:12 +0200

    18.11.2020

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:confesion_de_fe_de_westminster/detailPage.dart';
 import 'package:confesion_de_fe_de_westminster/dbhelper.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:share/share.dart';
 
 // https://proandroiddev.com/flutter-thursday-02-beautiful-list-ui-and-detail-page-a9245f5ceaf0
 // https://www.linkedin.com/pulse/to-do-list-flutter-sqlite-local-database-udara-abeythilake
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
@@ -37,45 +38,46 @@ class MyHomePage extends StatefulWidget{
 }
 
 class _MyListViewBuilder extends State<MyHomePage> {
-
   DBProvider dbProvider = DBProvider();
   List<Chapter> chapters;
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-
     if (chapters == null) {
       chapters = List<Chapter>();
       updateChapters();
     }
 
-    // final makeBottom = Container(
-    //   height: 55.0,
-    //   child: BottomAppBar(
-    //     color: Color.fromRGBO(58, 66, 86, 1.0),
-    //     child: Row(
-    //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //       children: <Widget>[
-    //         IconButton(
-    //           icon: Icon(Icons.home, color: Colors.white),
-    //           onPressed: () {},
-    //         ),
-    //         IconButton(
-    //           icon: Icon(Icons.blur_on, color: Colors.white),
-    //           onPressed: () {},
-    //         ),
-    //         IconButton(
-    //           icon: Icon(Icons.hotel, color: Colors.white),
-    //           onPressed: () {},
-    //         ),
-    //         IconButton(
-    //           icon: Icon(Icons.account_box, color: Colors.white),
-    //           onPressed: () {},
-    //         )
-    //       ],
-    //     ),
-    //   ),
-    // );
+    final makeBottom = Container(
+      height: 56.0,
+      child: BottomAppBar(
+        color: Color.fromRGBO(58, 66, 86, 1.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.share_rounded, color: Colors.yellowAccent),
+              onPressed: () {
+                Share.share('La Confesión de Fe Westminster https://example.com');
+              },
+            ),
+            // IconButton(
+            //   icon: Icon(Icons.blur_on, color: Colors.white),
+            //   onPressed: () {},
+            // ),
+            // IconButton(
+            //   icon: Icon(Icons.hotel, color: Colors.white),
+            //   onPressed: () {},
+            // ),
+            // IconButton(
+            //   icon: Icon(Icons.account_box, color: Colors.white),
+            //   onPressed: () {},
+            // )
+          ],
+        ),
+      ),
+    );
 
     ListTile makeListTile(chapters, int index) => ListTile(
         contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
@@ -112,14 +114,14 @@ class _MyListViewBuilder extends State<MyHomePage> {
           //   MaterialPageRoute(builder: (context) => DetailPage(chapters, index)),
           // );
           Navigator.push(
-              context, CupertinoPageRoute(builder: (context) =>
-              DetailPage(chapters, index))
-          );
+              context,
+              CupertinoPageRoute(
+                  builder: (context) => DetailPage(chapters, index)));
         });
 
     Card makeCard(chapters, int index) => Card(
           elevation: 8.0,
-          margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+          margin: new EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0),
           child: Container(
             decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
             child: makeListTile(chapters, index),
@@ -132,14 +134,14 @@ class _MyListViewBuilder extends State<MyHomePage> {
         shrinkWrap: true,
         itemCount: chapters == null ? 0 : chapters.length,
         itemBuilder: (BuildContext context, int index) {
-          return makeCard(chapters,index);
+          return makeCard(chapters, index);
         },
       ),
     );
 
     final topAppBar = AppBar(
       elevation: 0.1,
-      backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
+      backgroundColor: Color.fromRGBO(64, 75, 96, .9),
       title: Text(widget.title),
       //actions: <Widget>[
       //  IconButton(
@@ -149,13 +151,47 @@ class _MyListViewBuilder extends State<MyHomePage> {
       //],
     );
 
+    // Navigation Rail
+    // final navigationRail = Row(
+    //   children: [
+    //     NavigationRail(
+    //       backgroundColor: Color.fromRGBO(64, 75, 96, .9),
+    //       selectedIndex: _selectedIndex,
+    //       onDestinationSelected: (int index) {
+    //         setState(() {
+    //           _selectedIndex = index;
+    //         });
+    //       },
+    //       labelType: NavigationRailLabelType.none,
+    //       minWidth: 48,
+    //       destinations: [
+    //         NavigationRailDestination(
+    //           icon: Icon(Icons.share, color: Colors.white,),
+    //           selectedIcon: Icon(Icons.share, color: Colors.white,),
+    //           label: Text('First'),
+    //         ),
+    //         NavigationRailDestination(
+    //           icon: Icon(Icons.star_border, color: Colors.white,),
+    //           selectedIcon: Icon(Icons.star, color: Colors.white,),
+    //           label: Text('Second'),
+    //         ),
+    //       ],
+    //     ),
+    //     VerticalDivider(thickness: 1, width: 1),
+    //     Expanded(
+    //       child: (
+    //           makeBody
+    //       ),
+    //     )
+    //   ],
+    // );
+
     return Scaffold(
       backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
       appBar: topAppBar,
       body: makeBody,
-      //bottomNavigationBar: makeBottom,
+      bottomNavigationBar: makeBottom,
     );
-
   }
 
   void updateChapters() {
